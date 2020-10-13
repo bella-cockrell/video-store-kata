4e2ddf62723cc011ad8ef86ea898853ebca17d30
bella-cockrell <arabella.cockrell@gmail.com>
2020-10-13 10:57:28 +0100

    Customer name, test passes

--- lib/video_store.rb
+++ lib/video_store.rb
@@ -1,7 +1,7 @@
 class VideoStore
     def print_receipt(customer_name, movie_list)
         print <<~RECEIPT
-        Rental Record for 
+        Rental Record for #{customer_name}
         
         You owe 0.0
         You earned 0 frequent renter points
--- spec/video_store_spec.rb
+++ spec/video_store_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/video_store"
 
 describe VideoStore do
-    context "when I pass in two arguments" do
+    context "when I pass in two empty arguments" do
         it "prints the receipt" do
             #Arrange
             video_store = VideoStore.new
@@ -14,6 +14,21 @@ describe VideoStore do
                 RECEIPT
             #Assert
             expect{video_store.print_receipt("", [])}.to output(receipt).to_stdout
+        end
+    end
+    context "when I pass in the customer name" do
+        it "prints the receipt with the customer name" do
+            #Arrange
+            video_store = VideoStore.new
+            #Act
+            receipt = <<~RECEIPT
+                Rental Record for Bella
+                
+                You owe 0.0
+                You earned 0 frequent renter points
+                RECEIPT
+            #Assert
+            expect{video_store.print_receipt("Bella", [])}.to output(receipt).to_stdout
         end
     end
 end
